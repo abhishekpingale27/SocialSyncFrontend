999334c6485d41f611010c9ef2e1cc9a7d2c0f67
abhishekpingale27 <124373173+abhishekpingale27@users.noreply.github.com>
2025-05-21 10:01:09 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://socialsyncbackend-qe4w.onrender.com/api",
 });
 
 // Add request interceptor to include JWT
